70b2e19af329ea0ba8cdd40c1b4a43b2ce662d30
Igor Gonçalves <igor1788@gmail.com>
2023-06-15 15:22:27 -0300

    Add cupom function to concatenate the itens with no error

--- src/components/FormUTM.tsx
+++ src/components/FormUTM.tsx
@@ -6,15 +6,15 @@ const FormUtm = () => {
   const [origem, setOrigem] = useState('');
   const [midia, setMidia] = useState('');
   const [campanha, setCampanha] = useState('');
-  const [cupom, setCupom] = useState(false);
+  const [cupom, setCupom] = useState('');
   const [linkGerado, setLinkGerado] = useState('');
-  const [linkCopiado, setLinkCopiado] = useState(false); // Adicionado para controlar se o link foi copiado
+  const [linkCopiado, setLinkCopiado] = useState(false);
 
   const handleGenerateLink = () => {
-    let base = cupom ? `${url}&` : `${url}?`;
+    let base = cupom ? `${url}?voucher=${cupom}&` : `${url}?`; 
     base += `utm_source=${origem}&utm_medium=${midia}&utm_campaign=${campanha}`;
     setLinkGerado(base);
-    setLinkCopiado(false); // Resetamos a variável linkCopiado
+    setLinkCopiado(false);
   };
 
   const handleCopyLink = async () => {
@@ -27,14 +27,17 @@ const FormUtm = () => {
       }
     }
   }
+
   const handleClearFields = () => {
     setUrl('');
     setOrigem('');
     setMidia('');
     setCampanha('');
-    setCupom(false);
+    setCupom('');
     setLinkGerado('');
     setLinkCopiado(false);
+    console.log('Estou testando a zerada');
+    
   }
 
   return (
@@ -42,22 +45,19 @@ const FormUtm = () => {
       <h2>Gere a UTM aqui</h2>
       <div className="input-container">
         <label>URL: </label>
-        <input type="text" onChange={(e) => setUrl(e.target.value)} />
+        <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} />
 
         <label>Origem: </label>
-        <input type="text" onChange={(e) => setOrigem(e.target.value)} />
+        <input type="text" value={origem} onChange={(e) => setOrigem(e.target.value)} />
 
         <label>Mídia: </label>
-        <input type="text" onChange={(e) => setMidia(e.target.value)} />
+        <input type="text" value={midia} onChange={(e) => setMidia(e.target.value)} />
 
         <label>Campanha: </label>
-        <input type="text" onChange={(e) => setCampanha(e.target.value)} />
+        <input type="text" value={campanha} onChange={(e) => setCampanha(e.target.value)} />
 
-        <label>Cupom: </label>
-        <select onChange={(e) => setCupom(e.target.value === 'sim')}>
-          <option value="não">Não</option>
-          <option value="sim">Sim</option>
-        </select>
+        <label>Cupom: </label> 
+        <input type="text" value={cupom} onChange={(e) => setCupom(e.target.value)} />
 
         <button onClick={handleGenerateLink}>Gerar link</button>
         <button onClick={handleClearFields}>Limpar campos</button>
@@ -65,8 +65,8 @@ const FormUtm = () => {
 
       <div className='result-container'>
         <p 
-          onClick={handleCopyLink} // Adicionado evento de click para copiar o link
-          style={{cursor: "pointer"}} // Adicionado para mudar o cursor quando passar o mouse sobre o link
+          onClick={handleCopyLink}
+          style={{cursor: "pointer"}}
         >
           Link gerado: {linkGerado}
         </p>
